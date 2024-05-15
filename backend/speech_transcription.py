79bd53d9c3fa5a7c4387f5277bae1aca0d563d64
Tiaiba Nabil <40002528+nabirudesu@users.noreply.github.com>
2024-05-16 00:40:51 +0100

    adding azure openai whisper

--- backend/speech_transcription.py
+++ backend/speech_transcription.py
@@ -3,7 +3,16 @@ import numpy as np
 import Levenshtein
 import re
 from numbers_to_words import dinar_number_to_arabic_words
+from openai import AzureOpenAI
+    
+client = AzureOpenAI(
+    api_key="your_api_key",  
+    api_version="2024-02-01",
+    azure_endpoint = "your_azure_endpoint"
+)
 
+deployment_id = "voice-transcription" #This will correspond to the custom name you chose for your deployment when you deployed a model."
+audio_test_file = "backend_audio.wav"
 def Similarity_correction(transcript, word_list):
     replaced_sentence = []
     for word in transcript.split():
@@ -22,23 +31,25 @@ def Similarity_correction(transcript, word_list):
 
 # data to use for correction
 def arabic_audio_transcript(filename):
-    # Loading the model
-    model = whisper.load_model("medium")
-
     # Transcribe the audio
     try:
-        result = model.transcribe(filename,language='ar',fp16=False,verbose=True)
-        print("Transcription:", result["text"])
-        audio_transcript= result["text"]
+        # Loading the model
+        result = client.audio.transcriptions.create(file=open(filename, "rb"), model=deployment_id)
+        print("Transcription:", result.text)
+        audio_transcript= result.text
         word_list =['و','جزائري','دينار','واحد','اثنان','ثلاثة','أربعة','خمسة','ستة','سبعة','ثمانية','تسعة','عشرة','أحد عشر','اثنا عشر','عشر','عشرون','ثلاثون','أربعون','خمسون','ستون','سبعون','ثمانون','تسعون','مائة','مئتان','ثلاثمائة','أربعمائة','خمسمائة','ستمائة','سبعمائة','ثمانمائة','تسعمائة','ألف','الفان','ألفا','آلاف','مليار','ملياران','مليارات','مليون','مليونان','ملايين']
 
         #similarity_scores=[]
-        numbers_in_result= re.findall(r'\d+', audio_transcript)
-        for i in numbers_in_result:
-            converted_numbers= dinar_number_to_arabic_words(int(i))
-            text_audio_trascript=re.sub(r'\d+',converted_numbers,audio_transcript)
-        final_transcript = Similarity_correction(text_audio_trascript,word_list)
+        if any(char.isdigit() for char in audio_transcript):
+            numbers_in_result= re.findall(r'\d+', audio_transcript)
+            for i in numbers_in_result:
+                converted_numbers= dinar_number_to_arabic_words(int(i))
+                text_audio_trascript=re.sub(r'\d+',converted_numbers,audio_transcript)
+            final_transcript = Similarity_correction(text_audio_trascript,word_list)
+        else :
+            final_transcript = Similarity_correction(audio_transcript,word_list)
     except Exception as e:
+        print(e)
         return "التسجيل الصوتي خاطئ أو غير قابل للتحويل"
     return(final_transcript)
 
